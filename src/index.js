c84533eb350ab7a85ca1f3255115573ff683bcf9
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-06 20:11:04 +0300

    add tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // const tasks = document.getElementById("tasks");
+  // tasks.textContent = "Create New Task";
 });
+document.getElementById("create-task-form").addEventListener("submit", event => {
+    event.preventDefault();
+  });
+
+
+document.getElementById("create-task-form").addEventListener("click", event => {
+  if (event.target.tagName === "li") {
+    event.target.remove();
+  }
+});
+
+const createTaskForm = document.getElementById("create-task-form");
+const priority = document.getElementById("priority");
+const description = document.getElementById("description");
+
+
+function createNewTask() {
+  if(description.value === ""){
+    alert("Please, write something!")
+  }
+
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = description.value;
+    tasks.appendChild(li);
+  }
+
+}
