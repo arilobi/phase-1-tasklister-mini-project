5f0f972174eb5c9c5aea5553102d0975ed005e81
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-10 18:19:52 +0300

    add tasks

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // const tasks = document.getElementById("tasks");
-  // tasks.textContent = "Create New Task";
-});
-document.getElementById("create-task-form").addEventListener("submit", event => {
-    event.preventDefault();
-  });
+// I'm making my button to work when the user clicks on it.
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
 
+// I'm enabling the user to write inside the input section.
+let inputField = document.getElementById('inputField');
 
-document.getElementById("create-task-form").addEventListener("click", event => {
-  if (event.target.tagName === "li") {
-    event.target.remove();
-  }
-});
+// I created a paragraph so that when the user keys in their to do list, it appears below the input
+addToDoButton.addEventListener('click', (event) => {
+  let paragraph = document.createElement('p');
 
-const createTaskForm = document.getElementById("create-task-form");
-const priority = document.getElementById("priority");
-const description = document.getElementById("description");
+  // To specify the value of the text being inputed.
+  paragraph.innerText = inputField.value;
 
+  // We're adding a new child node which is paragraph to an existing parent node which is the toDoContainer.
+  toDoContainer.appendChild(paragraph)
 
-function createNewTask() {
-  if(description.value === ""){
-    alert("Please, write something!")
-  }
+  // I want the browser to delete the previous text in the input section after the user puts in the task.
+  inputField.value = "";
 
-  else{
-    let li = document.createElement("li");
-    li.innerHTML = description.value;
-    tasks.appendChild(li);
-  }
+  // Here, I want the user to be able to click off their task after completing it.
+  paragraph.addEventListener('click', (event) => {
+    paragraph.style.textDecoration = "line-through";
+  } )
+})
 
-}
+// The prevent default will prevent the submit button which is the "+" in my html from submitting. I'm not entirely sure what this actually does... 
+// ... but I just put it here cause I was told to do it. Lol!
+document.getElementById("container").addEventListener("addToDo", event => {
+  event.preventDefault();
+})
